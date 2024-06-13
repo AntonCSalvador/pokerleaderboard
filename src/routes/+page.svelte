58f8bb82cc7afa2270ca38a8a010f30f45567a6f
AntonCSalvador <antoncsalvador@gmail.com>
2024-06-13 10:58:54 -0400

    basic framework

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<div class="bg-[url('https://i.ibb.co/jWjTnBX/pokerwolves.png')] bg-cover bg-center min-h-screen flex flex-col items-center justify-center text-white">
+  <h1 class="text-4xl mb-4">CFCY POKER LEADERBOARD</h1>
+  <p class="text-xl">
+    Click <a class="underline text-blue-300" href="/leaderboard">kit.svelte.dev</a> to look at who is up and down
+  </p>
+</div>
---
+++ src/routes/leaderboard/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+
+</script>
+
+<div class="w-full h-16 bg-red-500 flex items-center justify-center">
+  <h1 class="text-4xl font-bold">JAX ALPHA POKER PLAYERS</h1>
+</div>
+
+<div class="bg-[url('https://i.ibb.co/jWjTnBX/pokerwolves.png')] bg-cover bg-center min-h-screen flex flex-col items-center justify-center text-white">
+  <div class="flex items-center justify-center mt-8 w-full">
+    <div class="border-b-2 border-blue-500 w-3/4">
+      <div class="flex items-center justify-center overflow-y-auto grow h-full">
+        <div id="searched-container" class="overflow-auto text-white bg-red-500 border border-blue-500 rounded-lg pt-5 pb-5 pr-4 pl-4 w-full mb-4" style="height: 325px;">
+          <div class="flex items-center justify-center">
+            <div class="bg-white text-black p-2 h-12 flex justify-center items-center">-</div>
+            <div class="bg-white text-black p-2">Column 2, Row 1</div>
+            <div class="bg-white text-black p-2">Column 3, Row 1</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
